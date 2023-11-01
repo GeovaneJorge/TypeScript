d0edc4245755d4f142d319c2af6b7fd52a48ce70
Geovane <GEOVANNE.JORGE@OUTLOOK.COM.BR>
2023-11-01 16:28:15 -0300

    Variaves e Objestos

---
+++ objeto.ts
@@ -0,0 +1,20 @@
+//Objetos sem previsibilidade 
+let produto:object={
+    name:"Geovane",
+    cidade:"RJ",
+    idade:30,
+};
+
+
+//Objeto com previsibilidade 
+type produtoLoja ={
+    nome:string;
+    preco:number;
+    unidades:number;
+}
+
+let meuProduto: produtoLoja={
+    nome:"Tênis",
+    preco: 300,
+    unidades:1,
+};
---
+++ variaves.ts
@@ -0,0 +1,13 @@
+//tipos primitivo :boolean, number, string
+let ligado: boolean = false;
+let nome: string = "João"
+let idade: number = 27;
+let altura: number = 1.75;
+
+// tipos especiais null, undefined 
+let numlo: null = null;
+let indefinido: undefined = undefined;
+
+//tipos abrangentes: any, void
+let retorno:void;
+let retorno:any = false; 
