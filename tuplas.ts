ea4ef7a415efb7dbc29b204173812054448d73f7
Geovane <GEOVANNE.JORGE@OUTLOOK.COM.BR>
2023-11-02 00:13:36 -0300

    tuplas

---
+++ tuplas.ts
@@ -0,0 +1,8 @@
+//A tupla Respeita o padrão das informações string, number, number . Diferente de um array multi types
+
+//Exemplo de tupla
+let boletos :[string, number, number]= ["agua conta", 199.9, 123456 ]
+
+
+//Exemplo de array multi types
+//let infos: (string | number)[]=["Maça", 9];
