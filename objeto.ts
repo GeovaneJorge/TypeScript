e1334ca2c56bb37f386abcbf0b2d306c2cf22f3f
Geovane <GEOVANNE.JORGE@OUTLOOK.COM.BR>
2023-11-01 23:09:32 -0300

    Parâmetros Modificados

---
+++ objeto.ts
@@ -0,0 +1,22 @@
+//Objetos sem previsibilidade 
+let produto:object={
+    name:"Geovane",
+    cidade:"RJ",
+    idade:30,
+    peso:70
+};
+
+
+//Objeto com previsibilidade 
+type produtoLoja ={
+    nome:string;
+    preco:number;
+    unidades:number;
+    peso:70;
+}
+
+let meuProduto: produtoLoja={
+    nome:"Tênis",
+    preco: 300,
+    unidades:1,
+};
