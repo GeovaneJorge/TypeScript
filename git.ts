e1e0f640c24a11bc576ad4067b46288e9b17b9b8
Geovane <33160858+GeovaneJorge@users.noreply.github.com>
2023-11-03 16:34:11 -0300

    Update git.ts

--- git.ts
+++ git.ts
@@ -4,9 +4,10 @@ git add README.md
 git commit -m "first commit"
 
 
-git add 
-git commit -m "array_e_Multi_Types" 
+git pull 
+git add .\funcoes.ts
+git commit -m "Funcoes" 
 git branch -M main
-git remote add origin https://github.com/GeovaneJorge/TypeScript.git
+git remote add origin https://github.com/GeovaneJorge/TypeScript/blob/main/git.ts
 git push -u origin main
 
