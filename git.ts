5c248e9df4295fdb7af96241cf3e9d9cfbb7521a
Geovane <GEOVANNE.JORGE@OUTLOOK.COM.BR>
2023-11-03 16:16:23 -0300

    Funcoes

--- git.ts
+++ git.ts
@@ -2,9 +2,11 @@ echo "# TypeScript" >> README.md
 git init
 git add README.md
 git commit -m "first commit"
+
+
+git add 
+git commit -m "array_e_Multi_Types" 
 git branch -M main
 git remote add origin https://github.com/GeovaneJorge/TypeScript.git
-git push -u origin main
-git push -u origin main
 git push -u origin main
 
